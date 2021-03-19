45a6df49488faf78f466f32ef472bf7656ce82ca
Paulo Martins <paulo.martins@zenvia.com>
2021-03-19 01:41:51 -0300

    components: app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Login from './views/Login'
+import ForgotPassword from './views/ForgotPassword'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="app-container">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/forgot-password" exact component={ForgotPassword} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
